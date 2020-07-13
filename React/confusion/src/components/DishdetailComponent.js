a73387c3d1dd901b5fe91ce7c0001b2db04c137d
Harsh Kumar <harsh19043@iiitd.ac.in>
2020-07-13 23:26:02 +0530

    Fetch from Server

--- React/confusion/src/components/DishdetailComponent.js
+++ React/confusion/src/components/DishdetailComponent.js
@@ -4,6 +4,7 @@ import {Card, CardBody, CardImg, CardText, CardTitle, Breadcrumb, Label,
 import { Link } from 'react-router-dom';
 import { LocalForm, Control, Errors } from 'react-redux-form';
 import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
 
 const minLength = (len) => (val) => !(val) || (val.length >=len);
 const maxLength = (len) => (val) => !(val) || (val.length <=len);
@@ -107,7 +108,7 @@ class CommentForm extends Component {
 function RenderDish({dish}) {
     return (
         <Card>
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImg width="100%" src={baseUrl+dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>
