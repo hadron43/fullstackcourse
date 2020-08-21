c88ee0dc25fb016d3d1a3817f6aaaada778745e9
Harsh Kumar <harsh19043@iiitd.ac.in>
2020-08-22 00:11:10 +0530

    Passport Facebook

--- NodeJS/conFusionServer/config.js
+++ NodeJS/conFusionServer/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/conFusion'
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: 4396593373714851,
+        clientSecret: '5b2736884ce48c1a1b2f024019e1c978'
+    }
 }
